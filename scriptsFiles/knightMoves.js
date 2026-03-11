cec6a819e7ff10dbf6ac5886180a0cbcb1f49699
Oreus-F <fourniol.lucas@gmail.com>
2026-03-11 16:34:32 +0100

    put some test in it

--- scriptsFiles/knightMoves.js
+++ scriptsFiles/knightMoves.js
@@ -1,6 +1,10 @@
-import { Graph } from "./node_tree";
+import { Graph } from "./node_tree.js";
 
 function knightMoves(cD, cA){
     const board = new Graph(cD, cA);
     board.getShortestPath();
 }
+
+
+knightMoves([0,0], [3,3]);
+knightMoves([0,0], [7,7]);
