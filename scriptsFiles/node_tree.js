2d2a9c26ff3bc926cbf01040b6b4d54ed76d4605
Oreus-F <fourniol.lucas@gmail.com>
2026-03-11 14:30:54 +0100

    create getPosition X Y and findPaths method

--- scriptsFiles/node_tree.js
+++ scriptsFiles/node_tree.js
@@ -2,30 +2,44 @@ export class Node{
     constructor(x, y){
         this.x = x,
         this.y = y,
+        this.positions = [this.x, this.y],
         this.nextPos = [],
         this.parent = []
     }
 
+    getX(){
+        return this.x
+    };
+
+    
+    getY(){
+        return this.y
+    };
+
+
+    getPositions(){
+        return this.positions
+    }
 
     caculateAllNextPost(){
         for(let a = 1; a < 3; a++){
             if(a === 1){
-                const addPlusTwo = this.calculateThisPosition(this.x + a, this.y + 2);
-                const addMinusTwo = this.calculateThisPosition(this.x + a, this.y - 2);
+                const addPlusTwo = this.calculateThisPosition(this.getX() + a, this.getY() + 2);
+                const addMinusTwo = this.calculateThisPosition(this.getX() + a, this.getY() - 2);
 
-                const subPlusTwo = this.calculateThisPosition(this.x - a, this.y + 2);
-                const subMinusTwo = this.calculateThisPosition(this.x - a, this.y - 2);
+                const subPlusTwo = this.calculateThisPosition(this.getX() - a, this.getY() + 2);
+                const subMinusTwo = this.calculateThisPosition(this.getX() - a, this.getY() - 2);
 
                 if(addPlusTwo !== null) this.nextPos.push(addPlusTwo);
                 if(addMinusTwo !== null) this.nextPos.push(addMinusTwo);
                 if(subPlusTwo !== null) this.nextPos.push(subPlusTwo);
                 if(subMinusTwo !== null) this.nextPos.push(subMinusTwo);
             } else {
-                const addPlusOne = this.calculateThisPosition(this.x + a, this.y + 1);
-                const addMinusOne = this.calculateThisPosition(this.x + a, this.y - 1);
+                const addPlusOne = this.calculateThisPosition(this.getX() + a, this.getY() + 1);
+                const addMinusOne = this.calculateThisPosition(this.getX() + a, this.getY() - 1);
 
-                const subPlusOne = this.calculateThisPosition(this.x - a, this.y + 1);
-                const subMinusOne = this.calculateThisPosition(this.x - a, this.y - 1);
+                const subPlusOne = this.calculateThisPosition(this.getX() - a, this.getY() + 1);
+                const subMinusOne = this.calculateThisPosition(this.getX() - a, this.getY() - 1);
 
                 if(addPlusOne !== null) this.nextPos.push(addPlusOne);
                 if(addMinusOne !== null) this.nextPos.push(addMinusOne);
@@ -42,13 +56,56 @@ export class Node{
 };
 
 export class Graph{
-    constructor(root){
-        this.root = root
+    constructor(root, target){
+        this.root = root,
+        this.target = target,
+        this.visited = {},
+        this.paths = {
+            'length': -Infinity,
+            'paths': []
+        }
     }
+
+    // checkPositions(node){
+    //     console.log(node)
+    // }
+
+    findPaths(queue = [this.root]){
+        while(queue.length > 0){
+            const node = queue[0];
+
+            if(node.getPositions() === this.target){
+                continue
+            }
+
+            const nextPositions = node.nextPos;
+
+            for(let x = 0; x < nextPositions.length; x++){
+                const positionskey = `${nextPositions[x][0]}${nextPositions[x][1]}`;
+                
+                if(!this.visited[positionskey]){
+                    const newN = new Node(nextPositions[x][0], nextPositions[x][1]);
+                    newN.parent.push(node)
+                    this.visited[positionskey] = newN
+
+                } else {
+                    console.log('nope')
+                }
+            }
+
+            queue.shift()
+        }
+
+        console.log(this.visited)
+    }
+
+
+
 }
 
 
-const firstPosition = new Node(4,4);
+const firstPosition = new Node(0,0);
 firstPosition.caculateAllNextPost();
 console.log(firstPosition.nextPos);
 const board = new Graph(firstPosition);
+board.findPaths()
